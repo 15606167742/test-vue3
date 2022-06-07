304274e572f460dd14b937ac0510e966db9b02c4
liujia <15606167742@163.com>
2022-06-07 12:56:44 +0800

    添加moment时间组件

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import moment from 'moment'
+Vue.prototype.$moment = moment
 
 createApp(App).use(store).use(router).mount('#app')
