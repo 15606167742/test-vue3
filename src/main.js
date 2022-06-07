4943e35488267ff9d24c9370ac84bb482804853e
liujia <15606167742@163.com>
2022-06-07 13:07:52 +0800

    vue原型绑定时间组件

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import moment from 'moment'
-Vue.prototype.$moment = moment
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.config.globalProperties.$moment = moment
+app.use(store).use(router).mount('#app')
